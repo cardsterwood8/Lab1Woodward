94ff1189a63cd51fba454e6fdfd3cfd55041983a
Richard Woodward <rwoodward2005@gmail.com>
2024-04-05 14:14:30 -0400

    first commit

---
+++ Lab1Woodward/Laboratory.cpp
@@ -0,0 +1,23 @@
+/*
+* Program:   <Lab1Woodward>
+* Author:    <Richard Woodward>
+* Data:      <1/24/2024>
+* 
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main(void) {
+	float width;
+	float length;
+	float area;
+	length = 4.5;
+	width = 3.35;
+	area = length * width;
+	cout << "Rectangle Area = " << area << "/n";
+	cout << "Who's on First? \n";
+		return 0;
+}
